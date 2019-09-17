2d4051275b68fa230ded4959c1bd8f261a88b971
Daniel Bright <brahyt@gmail.com>
2019-09-17 12:24:13 -0700

    added Reduce

--- danielReduce.js
+++ danielReduce.js
@@ -1,10 +1,12 @@
-function secretString(string){
-  let secretArray = string.split(" ");
-  let decoded = secretArray.reduce()
-  console.log(secretArray) 
-}
-
-
 const testInput = "noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest"
 
-secretString(testInput)
+function secretString(string){
+  let secretArray = string.split(" ");
+  let decoded = secretArray.reduce((acc, e) => {
+    if(e.length === 3) return (acc += ' ');
+    else return (acc += e.slice(-1));
+  },(str = ''));
+
+}
+
+secretString(testInput);
